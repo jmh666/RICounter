e9abdeaccc473701edbef3e61cc332b24fcd0fb7
Kevin Way <kway@monetate.com>
2015-04-30 17:36:11 -0400

    makes a number of minor changes: - change display order to be instance type - az instead of vice versa, to make   it easier to see when a need can be met by modifying an existing reservation   from one az to another - don't import unused libraries - don't bother handling execptions on get_all_reservations (instead just blow up) - let AWS filter out instances that aren't running - let AWS filter out reservations that aren't active

--- RICounter.py
+++ RICounter.py
@@ -7,35 +7,22 @@ negative balances indicate excess reserved instances
 positive balances indicate instances that are not falling under RIs
 """
 
-from collections import Counter;
-from os import getenv;
-import boto.ec2;
-import boto.rds;
+from collections import Counter
 
-DISABLED_REGIONS = ['cn-north-1', 'us-gov-west-1'];
+import boto.ec2
 
-regions = boto.ec2.regions();
+DISABLED_REGIONS = ['cn-north-1', 'us-gov-west-1']
 
-for region in regions:
-	if region.name in DISABLED_REGIONS:
-		continue;
+for region in [ r for r in boto.ec2.regions() if r.name not in DISABLED_REGIONS ]:
+	ec2 = region.connect()
 
-	try:
-		ec2 = region.connect();
+	reservations = ec2.get_all_reservations(filters={'instance-state-name':'running'})
+	instances = [i.instance_type + ' - ' + i.placement for r in reservations for i in r.instances]
 
-		reservations = ec2.get_all_reservations();
-	except Exception as e:
-		logging.warning("Could not get instance reservations for region: " + region.name + " (" + e.message + ")");
-		continue;
+	instance_counter = Counter(instances)
 
-
-	instances = [i.placement + ' - ' + i.instance_type for r in reservations for i in r.instances if i.state == 'running'];
-
-	instance_counter = Counter(instances);
-
-	for ri in ec2.get_all_reserved_instances():
-		if ri.state == 'active':
-			instance_counter.subtract({ri.availability_zone + ' - ' + ri.instance_type: ri.instance_count});
+	for ri in ec2.get_all_reserved_instances(filters={'state' : 'active'}):
+		instance_counter.subtract({ri.instance_type + ' - ' + ri.availability_zone : ri.instance_count})
 
 	for key in sorted(instance_counter.keys()):
-		print "%s\t%d" % (key, instance_counter[key]);
+		print "%s\t%d" % (key, instance_counter[key])
