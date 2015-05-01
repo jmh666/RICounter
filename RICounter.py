89ba12d21dc589a32cbc1deb61550f9536c0ed7c
Kevin Way <kway@monetate.com>
2015-05-01 13:25:29 -0400

    use defaultdicts for both running_instances and reserved_instances instead of using Counter for one and defaultdict for the other.

--- RICounter.py
+++ RICounter.py
@@ -8,7 +8,6 @@ positive balances indicate instances that are not falling under RIs
 """
 import argparse
 
-from collections import Counter
 from collections import defaultdict
 
 import boto.ec2
@@ -36,18 +35,20 @@ else:
 for region in regions:
 	ec2 = region.connect()
 
+	running_instances = defaultdict(int)
 	running = ec2.get_all_reservations(filters=instance_filters)
-	running_instances = [i.instance_type + "\t" + i.placement for r in running for i in r.instances]
-	running_instance_counter = Counter(running_instances)
+	for r in running:
+		for i in r.instances:
+			running_instances[i.instance_type + "\t" + i.placement] += 1
 
 	reserved_instances = defaultdict(int)
 	for ri in ec2.get_all_reserved_instances(filters=reservation_filters):
 		reserved_instances[ri.instance_type + "\t" + ri.availability_zone] += ri.instance_count
 
-	keys = set(reserved_instances.keys() + running_instance_counter.keys())
+	keys = set(reserved_instances.keys() + running_instances.keys())
 	if len(keys) == 0:
 		continue
 
 	for key in sorted(keys):
-		print "%s\t%d\t%d\t%d" % (key, running_instance_counter[key], reserved_instances[key], 
-			running_instance_counter[key] -  reserved_instances[key])
+		print "%s\t%d\t%d\t%d" % (key, running_instances[key], reserved_instances[key], 
+			running_instances[key] - reserved_instances[key])
