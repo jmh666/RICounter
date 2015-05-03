9e20dfe6e4d9b749cf4bca33dd38e3c0cfb6791b
Kevin Way <kway@monetate.com>
2015-05-03 17:32:00 -0400

    first pass of support for specifying multiple AWS profiles.  This is useful if you have a payer account and a child account, with all reservations kept in the payer account

--- RICounter.py
+++ RICounter.py
@@ -18,6 +18,8 @@ parser.add_argument('--region', action="append", dest="regions", help="specify r
 parser.add_argument('--no-ec2', action="store_false", dest="ec2", default=True, help="do not check EC2")
 parser.add_argument('--no-rds', action="store_false", dest="rds", default=True, help="do not check RDS")
 parser.add_argument('--no-redshift', action="store_false", dest="redshift", default=True, help="do not check Redshift")
+parser.add_argument('--profile', action="append", dest="profiles", default=None, help="specify AWS profile (s) (optional)")
+
 args = parser.parse_args()
 
 DISABLED_REGIONS = ['cn-north-1', 'us-gov-west-1']
@@ -51,18 +53,25 @@ else:
     regions['redshift'] = [r for r in boto.redshift.regions() if r.name in args.regions]
     regions['rds'] = [r for r in boto.rds2.regions() if r.name in args.regions]
 
+profiles = [None] if args.profiles is None else args.profiles
+
 if args.ec2:
     print "EC2 Reservation Report"
     print "Instance\tPlacement\tRun\tReserve\tDiff"
     for region in regions['ec2']:
-        ec2 = region.connect()
-
-        running = ec2.get_all_reservations(filters={'instance-state-name': 'running'})
-        running_instances = Counter([i.instance_type + "\t" + i.placement for r in running for i in r.instances])
-
+        running_instances = defaultdict(int)
         reserved_instances = defaultdict(int)
-        for ri in ec2.get_all_reserved_instances(filters={'state': ['active', 'payment-pending']}):
-            reserved_instances[ri.instance_type + "\t" + ri.availability_zone] += ri.instance_count
+
+        for profile in profiles:
+            ec2 = region.connect(profile_name=profile)
+
+            running = ec2.get_all_reservations(filters={'instance-state-name': 'running'})
+            for r in running:
+                for i in r.instances:
+                    running_instances[i.instance_type + "\t" + i.placement] += 1
+
+            for ri in ec2.get_all_reserved_instances(filters={'state': ['active', 'payment-pending']}):
+                reserved_instances[ri.instance_type + "\t" + ri.availability_zone] += ri.instance_count
 
         print_results(running_instances, reserved_instances)
 
@@ -71,18 +80,20 @@ if args.redshift:
     print "Redshift Reservation Report"
     print "NodeType\tRegion  \tRunning\tReserve\tDiff"
     for region in regions['redshift']:
-        conn = boto.redshift.connect_to_region(region.name)
-
         running_nodes = defaultdict(int)
-        cluster_response = conn.describe_clusters()
-        for cluster in cluster_response['DescribeClustersResponse']['DescribeClustersResult']['Clusters']:
-            running_nodes[cluster['NodeType'] + "\t" + region.name] += cluster['NumberOfNodes']
-
         reserved_nodes = defaultdict(int)
-        reservation_response = conn.describe_reserved_nodes()
-        active_reservations = [x for x in reservation_response['DescribeReservedNodesResponse']['DescribeReservedNodesResult']['ReservedNodes'] if x['State'] in ('active', 'payment-pending')]
-        for reservation in active_reservations:
-            reserved_nodes[reservation['NodeType'] + "\t" + region.name] += reservation['NodeCount']
+
+        for profile in profiles:
+            conn = boto.redshift.connect_to_region(region.name, profile_name=profile)
+
+            cluster_response = conn.describe_clusters()
+            for cluster in cluster_response['DescribeClustersResponse']['DescribeClustersResult']['Clusters']:
+                running_nodes[cluster['NodeType'] + "\t" + region.name] += cluster['NumberOfNodes']
+
+            reservation_response = conn.describe_reserved_nodes()
+            active_reservations = [x for x in reservation_response['DescribeReservedNodesResponse']['DescribeReservedNodesResult']['ReservedNodes'] if x['State'] in ('active', 'payment-pending')]
+            for reservation in active_reservations:
+                reserved_nodes[reservation['NodeType'] + "\t" + region.name] += reservation['NodeCount']
 
         print_results(running_nodes, reserved_nodes)
 
@@ -91,15 +102,19 @@ if args.rds:
     print "RDS Reservation Report"
     print "Instance\tDB\tMultiAZ\tRegion  \tRunning\tReserve\tDiff"
     for region in regions['rds']:
-        conn = boto.rds2.connect_to_region(region.name)
-
-        rdb_response = conn.describe_db_instances()
-        running_rds = Counter([db['DBInstanceClass'] + "\t" + db['Engine'] + "\t" + str(db['MultiAZ']) + "\t" + region.name for db in rdb_response['DescribeDBInstancesResponse']['DescribeDBInstancesResult']['DBInstances']])
-
+        running_rds = defaultdict(int)
         reserved_rds = defaultdict(int)
-        reservation_response = conn.describe_reserved_db_instances()
-        active_reservations = [x for x in reservation_response['DescribeReservedDBInstancesResponse']['DescribeReservedDBInstancesResult']['ReservedDBInstances'] if x['State'] in ('active', 'payment-pending')]
-        for r in active_reservations:
-            reserved_rds[r['DBInstanceClass'] + "\t" + r['ProductDescription'] + "\t" + str(db['MultiAZ']) + "\t" + region.name] += r['DBInstanceCount']
+
+        for profile in profiles:
+            conn = boto.rds2.connect_to_region(region.name, profile_name=profile)
+
+            rdb_response = conn.describe_db_instances()
+            for db in rdb_response['DescribeDBInstancesResponse']['DescribeDBInstancesResult']['DBInstances']:
+                running_rds[db['DBInstanceClass'] + "\t" + db['Engine'] + "\t" + str(db['MultiAZ']) + "\t" + region.name] += 1
+
+            reservation_response = conn.describe_reserved_db_instances()
+            active_reservations = [x for x in reservation_response['DescribeReservedDBInstancesResponse']['DescribeReservedDBInstancesResult']['ReservedDBInstances'] if x['State'] in ('active', 'payment-pending')]
+            for r in active_reservations:
+                reserved_rds[r['DBInstanceClass'] + "\t" + r['ProductDescription'] + "\t" + str(db['MultiAZ']) + "\t" + region.name] += r['DBInstanceCount']
 
         print_results(running_rds, reserved_rds)
