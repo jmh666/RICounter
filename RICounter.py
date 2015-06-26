af0c01549d84b8a1c5d207b3ff602de0cbd1180c
Kevin Way <kway@monetate.com>
2015-06-25 21:29:43 -0400

    add configurable  delimiter

--- RICounter.py
+++ RICounter.py
@@ -20,6 +20,7 @@ parser.add_argument('--no-rds', action="store_false", dest="rds", default=True, 
 parser.add_argument('--no-redshift', action="store_false", dest="redshift", default=True, help="do not check Redshift")
 parser.add_argument('--only-variance', action="store_true", dest="only_variance", default=False, help="only output lines with a variance")
 parser.add_argument('--profile', action="append", dest="profiles", default=None, help="specify AWS profile (s) (optional)")
+parser.add_argument('--delimiter', action="store", dest="delimiter", default="\t", help="specify output delimiter (default: tab)")
 
 args = parser.parse_args()
 
@@ -38,12 +39,12 @@ def sort_instances(instances):
     return sorted(instances, key=instance_key)
 
 
-def print_results(running, reserved):
+def print_results(running, reserved, delimiter):
     keys = set(running.keys() + reserved.keys())
     for key in sort_instances(keys):
         variance = running[key] - reserved[key]
         if args.only_variance is False or abs(variance) > 0:
-            print "%s\t%d\t%d\t%d" % (key, running[key], reserved[key], running[key] - reserved[key])
+            print delimiter.join([key, str(running[key]), str(reserved[key]), str(running[key] - reserved[key])])
 
 
 regions = {}
@@ -60,7 +61,7 @@ profiles = [None] if args.profiles is None else args.profiles
 
 if args.ec2:
     print "EC2 Reservation Report"
-    print "Instance\tPlacement\tRun\tReserve\tDiff"
+    print args.delimiter.join(['Instance', 'Placement', 'Run', 'Reserve', 'Diff'])
     for region in regions['ec2']:
         running_instances = defaultdict(int)
         reserved_instances = defaultdict(int)
@@ -71,17 +72,17 @@ if args.ec2:
             running = ec2.get_all_reservations(filters={'instance-state-name': 'running'})
             for r in running:
                 for i in r.instances:
-                    running_instances[i.instance_type + "\t" + i.placement] += 1
+                    running_instances[i.instance_type + args.delimiter + i.placement] += 1
 
             for ri in ec2.get_all_reserved_instances(filters={'state': ['active', 'payment-pending']}):
-                reserved_instances[ri.instance_type + "\t" + ri.availability_zone] += ri.instance_count
+                reserved_instances[ri.instance_type + args.delimiter + ri.availability_zone] += ri.instance_count
 
-        print_results(running_instances, reserved_instances)
+        print_results(running_instances, reserved_instances, args.delimiter)
 
 # Redshift
 if args.redshift:
     print "Redshift Reservation Report"
-    print "NodeType\tRegion  \tRunning\tReserve\tDiff"
+    print args.delimiter.join(['NodeType', 'Region', 'Running', 'Reserve', 'Diff'])
     for region in regions['redshift']:
         running_nodes = defaultdict(int)
         reserved_nodes = defaultdict(int)
@@ -91,19 +92,19 @@ if args.redshift:
 
             cluster_response = conn.describe_clusters()
             for cluster in cluster_response['DescribeClustersResponse']['DescribeClustersResult']['Clusters']:
-                running_nodes[cluster['NodeType'] + "\t" + region.name] += cluster['NumberOfNodes']
+                running_nodes[cluster['NodeType'] + args.delimiter + region.name] += cluster['NumberOfNodes']
 
             reservation_response = conn.describe_reserved_nodes()
             active_reservations = [x for x in reservation_response['DescribeReservedNodesResponse']['DescribeReservedNodesResult']['ReservedNodes'] if x['State'] in ('active', 'payment-pending')]
             for reservation in active_reservations:
-                reserved_nodes[reservation['NodeType'] + "\t" + region.name] += reservation['NodeCount']
+                reserved_nodes[reservation['NodeType'] + args.delimiter + region.name] += reservation['NodeCount']
 
-        print_results(running_nodes, reserved_nodes)
+        print_results(running_nodes, reserved_nodes, args.delimiter)
 
 # RDS
 if args.rds:
     print "RDS Reservation Report"
-    print "Instance\tDB\tMultiAZ\tRegion  \tRunning\tReserve\tDiff"
+    print args.delimiter.join(['Instance', 'DB', 'MultiAZ', 'Region', 'Running', 'Reserve', 'Diff'])
     for region in regions['rds']:
         running_rds = defaultdict(int)
         reserved_rds = defaultdict(int)
@@ -113,11 +114,11 @@ if args.rds:
 
             rdb_response = conn.describe_db_instances()
             for db in rdb_response['DescribeDBInstancesResponse']['DescribeDBInstancesResult']['DBInstances']:
-                running_rds[db['DBInstanceClass'] + "\t" + db['Engine'] + "\t" + str(db['MultiAZ']) + "\t" + region.name] += 1
+                running_rds[db['DBInstanceClass'] + args.delimiter + db['Engine'] + args.delimiter + str(db['MultiAZ']) + args.delimiter + region.name] += 1
 
             reservation_response = conn.describe_reserved_db_instances()
             active_reservations = [x for x in reservation_response['DescribeReservedDBInstancesResponse']['DescribeReservedDBInstancesResult']['ReservedDBInstances'] if x['State'] in ('active', 'payment-pending')]
             for r in active_reservations:
-                reserved_rds[r['DBInstanceClass'] + "\t" + r['ProductDescription'] + "\t" + str(db['MultiAZ']) + "\t" + region.name] += r['DBInstanceCount']
+                reserved_rds[r['DBInstanceClass'] + args.delimiter + r['ProductDescription'] + args.delimiter + str(db['MultiAZ']) + args.delimiter + region.name] += r['DBInstanceCount']
 
-        print_results(running_rds, reserved_rds)
+        print_results(running_rds, reserved_rds, args.delimiter)
